c1904a1b140816dc4d424957d752dd182712c015
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-03-29 05:35:02 -0400

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 import requests
